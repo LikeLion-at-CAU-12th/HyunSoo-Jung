6bfc4d3f9fbbcdb52f1cc17ac68ec3381c9bbec0
usernamehs <hsforwork123@gmail.com>
2024-03-23 02:17:15 +0900

    feat: 챌린지 과제

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class myinfo(models.Model):
+    myname = models.CharField(default="", max_length = 10)
+    myage = models.IntegerField(default=0)
+    mymajor = models.CharField(default="", max_length = 50)
+    mygit = models.CharField(default="", max_length = 15)
+
+class reviewerinfo(models.Model):
+    reviewername = models.CharField(default="", max_length = 10)
+    reviewerage = models.IntegerField(default=0)
+    reviewermajor = models.CharField(default="", max_length = 50)
+    reviewergit = models.CharField(default="", max_length = 15)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, render
 from django.http import JsonResponse # 추가 
 from django.shortcuts import get_object_or_404 # 추가
+from .models import myinfo, reviewerinfo
+from .forms import myinfoform, reviewerinfoform
 
 # Create your views here.
 def hello_world(request):
@@ -14,13 +16,15 @@ def hello_world(request):
                     # 내 정보
                     "name" : "정현수",
                     "age" : 22,
-                    "major" : "LIS"
+                    "major" : "LIS",
+                    "git": "@usernamehs"
                 },
                 {
                     # 코드리뷰 짝 정보
                     "name" : "김동영",
                     "age" : 24,
-                    "major" : "CSE"
+                    "major" : "CSE",
+                    "git" : "@Temple2001"
                 }
             ]
         })
@@ -28,3 +32,12 @@ def hello_world(request):
     
 def index(request):
     return render(request, 'index.html')
+
+
+def myinfo_view(request):
+    myinfo_all = myinfo.objects.all()
+    return render(request, 'index.html', {"myinfo_list" : myinfo_all})
+
+def reviewerinfo_view(request):
+    reviewerinfo_all = reviewerinfo.objects.all()
+    return render(request, 'index.html', {"reviewer_list" : reviewerinfo_all})
